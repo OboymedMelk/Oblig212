a8177bc9abe4ec6b5f2cbec57060a83d060d587e
OboymedMelk <adrian.angeltveit@hotmail.com>
2022-11-01 10:41:30 +0100

    Update models.py

--- django-test/mysite/mysite/models.py
+++ django-test/mysite/mysite/models.py
@@ -1,12 +1,30 @@
 from django.db import models
 
+
 class Car(models.Model):
     make = models.CharField(max_length=50)
     carmodel = models.CharField(max_length=50)
-    year = models.IntergerField()
+    year = models.IntergerField(max_length=4)
     location = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.make + ' ' + self.carmodel + ' ' + self.year + ' ' + self.location + ' ' + self.status
 
-    def __str__(self): 
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntergerField()
+    address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.make + ' ' + self.carmodel
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    branch = models.IntergerField()
+
+    def __str__(self):
         return self.make + ' ' + self.carmodel
