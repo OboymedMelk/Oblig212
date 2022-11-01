eef13fd9fb9756c0a44c739ef9a4d8ae8908c540
OboymedMelk <adrian.angeltveit@hotmail.com>
2022-11-01 12:02:26 +0100

    Deleted database and string command on year. Now working

--- django-test/mysite/mysite/models.py
+++ django-test/mysite/mysite/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=50)
     carmodel = models.CharField(max_length=50)
-    year = models.IntegerField(max_length=4)
+    year = models.IntegerField()
     location = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.make + ' ' + self.carmodel + ' ' + self.year + ' ' + self.location + ' ' + self.status
+        return self.make + ' ' + self.carmodel + ' ' + str(self.year) + ' ' + self.location + ' ' + self.status
 
 
 class Customer(models.Model):
